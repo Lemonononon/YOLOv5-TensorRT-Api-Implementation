7c38dd4c6632b65a83738bedcbc985e225ffe468
xuejiejie <xuejiejie@xiaoyingai.com>
2024-12-27 14:51:33 +0800

    bakup

--- main.cpp
+++ main.cpp
@@ -830,7 +830,7 @@ void draw_bbox(std::vector<cv::Mat>& img_batch, std::vector<std::vector<Detectio
 int main(){
 
     // std::string engine_path = "../weights/yolov5s_platelet.engine";
-    std::string engine_path = "../weights/yolov5m_red_d.engine";
+    std::string engine_path = "../weights/yolov5m_red.engine";
     bool need_build = false;
     need_build = true;
 
